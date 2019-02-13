c2787257d3ae8e6bf52e4483edee4a80dd6dba7d
Jhemeson Silva Mota <jhemesonmotta@gmail.com>
2019-02-13 14:38:04 -0200

    Initial Commit

---
+++ WebApp/DAO/Entidades/Pedido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
---
+++ WebApp/DAO/MyWebAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Entidades;
+
+namespace WebApp.DAO
+{
+    public class MyWebAppContext : DbContext
+    {
+        // precisa do driver de conexão pro mySql
+            // nuguet console
+                // Install-Package Pomelo.EntityFramework.MySql
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> Itens { get; set; }
+    }
+}
