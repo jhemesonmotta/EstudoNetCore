14527edcd73f1732bfbcc8ad13ffaf99ca3046c9
Jhemeson Silva Mota <jhemeson.mota@crp.com>
2019-02-15 12:47:49 -0300

    adicionando migrations

--- WebApp/DAO/MyWebAppContext.cs
+++ WebApp/DAO/MyWebAppContext.cs
@@ -11,5 +11,28 @@ namespace WebApp.DAO
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<ItemPedido> Itens { get; set; }
+
+        public MyWebAppContext(DbContextOptions<MyWebAppContext> options):base(options)
+        {
+            // aqui eu defino que estou enviando para o base (ou seja, DBContext) as informações desta classe
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().HasKey(p => p.Id); // aqui informo q a tabela produto tem uma chave chamada "Id"
+            modelBuilder.Entity<ItemPedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>().HasKey(p => p.Id);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+
+        //Executa "Add-Migration MyWebAppMigration" para habilitar o recurso de migrations no projeto
     }
 }
