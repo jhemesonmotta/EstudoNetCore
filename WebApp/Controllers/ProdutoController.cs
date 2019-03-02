77b29ac7af9e870b5ebd9dfa897d3f33780ee72a
Jhemeson <jhemesonmotta@gmail.com>
2019-03-02 11:20:19 -0300

    iniciando a transformação de MVC para WebAPI

---
+++ WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApp.DAO;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        
+        private readonly IProdutoRepository _produtoRepository;
+
+        // injetando a dependencia pra evitar que eu tenha q instanciar a classe aqui dentro
+        public ProdutoController(IProdutoRepository produtoRepository) {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            try
+            {
+                return Ok(_produtoRepository.ListarProdutos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Erro: " + ex.Message);
+            }
+        }
+    }
+}
